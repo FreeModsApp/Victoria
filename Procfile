c559dcae3a402a3429ded7a0ccda1643384dce18
Legend Akshay <88607836+FreeModsApp@users.noreply.github.com>
2021-08-19 11:45:50 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m VictoriaBot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+TITLE Victoria Bot
+:: Enables virtual env mode and then starts Rika
+env\scripts\activate.bat && py -m VictoriaBot
+
